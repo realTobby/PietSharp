efe08bc71977f8129b47c8f0fa5bb868b96b4e48
xSillusx <tkattanek00@gmail.com>
2018-12-20 19:56:35 +0100

    Init

---
+++ PietSharp/PietSharp/PietInterpret.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PietSharp
+{
+    class PietInterpret
+    {
+        public int[] stack = null;
+        public Bitmap PietFile { get; set; }
+
+        public PietInterpret(string filepath)
+        {
+            PietFile = new Bitmap(filepath);
+        }
+
+        public void Start()
+        {
+            // get codel size (width and height)
+            // implement function with the different commands
+            // implement a way to stack the integers
+            // implement a way to detect the hue/light/color changes and "execute" the corresponding command
+        }
+
+
+
+    }
+}
--- PietSharp/PietSharp/Program.cs
+++ PietSharp/PietSharp/Program.cs
@@ -8,9 +8,16 @@ namespace PietSharp
 {
     class Program
     {
+        private static string buildVersion = "0.0";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("PietSharp v" + buildVersion);
+            Console.WriteLine("===============");
+            Console.Write("Path to piet code: ");
+            string inputPath = Console.ReadLine();
+            PietInterpret interpret = new PietInterpret(inputPath);
+            interpret.Start();
             Console.ReadKey();
         }
     }
