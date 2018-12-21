ec93ead66daaad6804cbf0afd16a34cbdea4ccd5
xSillusx <tkattanek00@gmail.com>
2018-12-21 13:32:34 +0100

    Update
    - codels are not assigned a hue and a light upon reading the pietfile

---
+++ PietSharp/PietSharp/CODEL.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PietSharp
+{
+    public class CODEL
+    {
+        public string HEXCOLOR { get; set; }
+        public int INDEX_X { get; set; }
+        public int INDEX_Y { get; set; }
+
+        public int BLOCK_SIZE_COUNT { get; set; }
+
+        public HUE_CYCLE COLOR_NAME { get; set; }
+        public LIGHT_CYCLE LIGHT { get; set; }
+
+        public CODEL(int x, int y, Color c)
+        {
+            INDEX_X = x;
+            INDEX_Y = y;
+            HEXCOLOR = c.R.ToString("X2") + c.G.ToString("X2") + c.B.ToString("X2");
+
+            DiffColorNameAndLight();
+        }
+
+        private void DiffColorNameAndLight()
+        {
+            switch(HEXCOLOR)
+            {
+                case "FFC0C0":
+                    COLOR_NAME = HUE_CYCLE.RED;
+                    LIGHT = LIGHT_CYCLE.LIGHT;
+                    break;
+                case "FF0000":
+                    COLOR_NAME = HUE_CYCLE.RED;
+                    LIGHT = LIGHT_CYCLE.NORMAL;
+                    break;
+                case "C00000":
+                    COLOR_NAME = HUE_CYCLE.RED;
+                    LIGHT = LIGHT_CYCLE.DARK;
+                    break;
+
+                case "FFFFC0":
+                    COLOR_NAME = HUE_CYCLE.YELLOW;
+                    LIGHT = LIGHT_CYCLE.LIGHT;
+                    break;
+                case "FFFF00":
+                    COLOR_NAME = HUE_CYCLE.YELLOW;
+                    LIGHT = LIGHT_CYCLE.NORMAL;
+                    break;
+                case "C0C000":
+                    COLOR_NAME = HUE_CYCLE.YELLOW;
+                    LIGHT = LIGHT_CYCLE.DARK;
+                    break;
+
+                case "C0FFC0":
+                    COLOR_NAME = HUE_CYCLE.GREEN;
+                    LIGHT = LIGHT_CYCLE.LIGHT;
+                    break;
+                case "00FF00":
+                    COLOR_NAME = HUE_CYCLE.GREEN;
+                    LIGHT = LIGHT_CYCLE.NORMAL;
+                    break;
+                case "00C000":
+                    COLOR_NAME = HUE_CYCLE.GREEN;
+                    LIGHT = LIGHT_CYCLE.DARK;
+                    break;
+
+                case "C0FFFF":
+                    COLOR_NAME = HUE_CYCLE.CYAN;
+                    LIGHT = LIGHT_CYCLE.LIGHT;
+                    break;
+                case "00FFFF":
+                    COLOR_NAME = HUE_CYCLE.CYAN;
+                    LIGHT = LIGHT_CYCLE.NORMAL;
+                    break;
+                case "00C0C0":
+                    COLOR_NAME = HUE_CYCLE.CYAN;
+                    LIGHT = LIGHT_CYCLE.DARK;
+                    break;
+
+                case "C0C0FF":
+                    COLOR_NAME = HUE_CYCLE.BLUE;
+                    LIGHT = LIGHT_CYCLE.LIGHT;
+                    break;
+                case "0000FF":
+                    COLOR_NAME = HUE_CYCLE.BLUE;
+                    LIGHT = LIGHT_CYCLE.NORMAL;
+                    break;
+                case "0000C0":
+                    COLOR_NAME = HUE_CYCLE.BLUE;
+                    LIGHT = LIGHT_CYCLE.DARK;
+                    break;
+
+                case "FFC0FF":
+                    COLOR_NAME = HUE_CYCLE.MAGENTA;
+                    LIGHT = LIGHT_CYCLE.LIGHT;
+                    break;
+                case "FF00FF":
+                    COLOR_NAME = HUE_CYCLE.MAGENTA;
+                    LIGHT = LIGHT_CYCLE.NORMAL;
+                    break;
+                case "C000C0":
+                    COLOR_NAME = HUE_CYCLE.MAGENTA;
+                    LIGHT = LIGHT_CYCLE.DARK;
+                    break;
+                default:
+                    COLOR_NAME = HUE_CYCLE.WHITE;
+                    LIGHT = LIGHT_CYCLE.NORMAL;
+                    break;
+                case "FFFFFF":
+                    COLOR_NAME = HUE_CYCLE.WHITE;
+                    LIGHT = LIGHT_CYCLE.NORMAL;
+                    break;
+                case "000000":
+                    COLOR_NAME = HUE_CYCLE.BLACK;
+                    LIGHT = LIGHT_CYCLE.NORMAL;
+                    break;
+            }
+        }
+    }
+}
--- PietSharp/PietSharp/PietInterpret.cs
+++ PietSharp/PietSharp/PietInterpret.cs
@@ -7,22 +7,97 @@ using System.Threading.Tasks;
 
 namespace PietSharp
 {
+    enum DP
+    {
+        RIGHT,
+        LEFT,
+        UP,
+        DOWN,
+    }
+
+    enum CC
+    {
+        RIGHT,
+        LEFT
+    }
+
+    public enum HUE_CYCLE
+    {
+        RED,
+        YELLOW,
+        GREEN,
+        CYAN,
+        BLUE,
+        MAGENTA,
+        WHITE,
+        BLACK
+    }
+
+    public enum LIGHT_CYCLE
+    {
+        LIGHT,
+        NORMAL,
+        DARK
+    }
+
     class PietInterpret
     {
-        public int[] stack = null;
+        public Stack<int> stack = new Stack<int>();
         public Bitmap PietFile { get; set; }
+
+        public List<CODEL> PietCode { get; set; }
+
+        public int CURRENT_ATTEMPTS = 0;
+
+        public DP DIRECTION_POINTER { get; set; } = DP.RIGHT;
+        public CC CODEL_CHOOSER { get; set; } = CC.LEFT;
+
+        public int POINTER_X { get; set; } = 0;
+        public int POINTER_Y { get; set; } = 0;
 
         public PietInterpret(string filepath)
         {
             PietFile = new Bitmap(filepath);
+            PietCode = new List<CODEL>();
         }
 
         public void Start()
         {
-            // get codel size (width and height)
-            // implement function with the different commands
-            // implement a way to stack the integers
-            // implement a way to detect the hue/light/color changes and "execute" the corresponding command
+            // === program execution === 
+            // set codelSize to correspond to the size of the colored blocks
+            // create a way to determine where the DIRECTION_POINTER AND CODEL_CHOOSER moves
+            // add numbers to stack
+            // black blocks and edges
+            // white blocks
+            // commands
+            Console.Write("Codel Width: ");
+            int codelWidth = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Codel height: ");
+            int codelHeight = Convert.ToInt32(Console.ReadLine());
+
+            // read every codel and save color into color canvas
+            for(int codel_y = 0; codel_y < PietFile.Size.Height; codel_y += codelHeight)
+            {
+                for(int codel_x = 0; codel_x < PietFile.Size.Width; codel_x+=codelWidth)
+                {
+                    Color middlePixel = PietFile.GetPixel(codel_x + codelWidth / 2, codel_y + codelHeight / 2);
+                    PietCode.Add(new CODEL(codel_x, codel_y, middlePixel));
+                }
+            }
+
+            while(CURRENT_ATTEMPTS < 8)
+            {
+
+            }
+
+
+
+
+
+
+
+
         }
 
 
